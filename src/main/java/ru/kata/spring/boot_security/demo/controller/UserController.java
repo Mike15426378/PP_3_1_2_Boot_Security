b61ec24ffb3f692808de88c54e99ff66cee48f1c
Mike15426378 / <Ravaga228>
2022-03-04 10:46:53 +0300

    v4 все ок

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
@@ -13,12 +12,12 @@ import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
-public class RestUserController {
+public class UserController {
 
     private UserService userService;
 
     @Autowired
-    public RestUserController(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
